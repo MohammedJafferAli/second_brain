c4ac91b5d7d8f7ef79dc2dab58a0a6c8708e7405
mohammedjafferali <mohammedjafferali.abubakkarsiddiq@bt.com>
2025-05-21 14:30:12 +0530

    Optimised for env and device

--- buildApp_git.sh
+++ buildApp_git.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-cd /Users/mohammedjafferali.abubakkarsiddiq/Documents/myee-app || exit
+cd <project-folder> || exit
+
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <branch_name>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <branch_name> <arg_choice: env1|env2|prod> <device_name>"
     exit 1
 fi
 
 BRANCH_NAME=$1
-FLAVOR="eesafe"
+ARG_CHOICE=$2
+DEVICE_NAME=$3
+
 
 # Fetch and checkout the branch
 git fetch origin "$BRANCH_NAME"
@@ -27,7 +30,6 @@ else
 fi
 
 # List available iOS simulators matching the device name
-DEVICE_NAME="iPhone 15 Pro"
 echo "Available iOS simulators matching: $DEVICE_NAME"
 xcrun simctl list devices available | grep -E "$DEVICE_NAME" | awk -F '[()]' '{print $2 " - " $1}'
 
@@ -35,7 +37,7 @@ xcrun simctl list devices available | grep -E "$DEVICE_NAME" | awk -F '[()]' '{p
 SIMULATOR_ID=$(xcrun simctl list devices available | grep -E "$DEVICE_NAME" | awk -F '[()]' '{print $2}' | head -n 1)
 
 if [ -z "$SIMULATOR_ID" ]; then
-    echo "❌ No available iOS 17.5 simulators found."
+    echo "❌ No available iOS $DEVICE_NAME simulators found."
     exit 1
 fi
 
@@ -47,32 +49,32 @@ if [ "$SIMULATOR_STATE" != "(Booted)" ]; then
     sleep 10
 fi
 
-# Function to run the Flutter app with additional arguments
+# Function to run flutter app with additional arguments
 run_flutter_app() {
     local simulator_id=$1
     shift
     local additional_args="$@"
 
     echo "🚀 Running app on simulator $simulator_id with additional arguments: $additional_args"
-    flutter run --flavor "$FLAVOR" -d "$simulator_id" $additional_args
+    flutter run -d "$simulator_id" $additional_args
 }
 
 # Prompt user to select one of three predefined argument sets
 echo "Select the argument set to use:"
-echo "1) BF1"
-echo "2) BF2"
+echo "1) env1"
+echo "2) env2"
 echo "3) Prod"
 read -p "Enter the number of your choice: " ARG_CHOICE
 
 case $ARG_CHOICE in
-    bf1)
-        ARGS="--dart-define=BUILD_TYPE=enterprise --dart-define=FLAVOR_NAME=Bf1 --dart-define=AUTH_CUSTOM_SCHEME=eeflutter --dart-define-from-file=assets/env_files_dev/env_bf1.json"
+    env1)
+        ARGS="--dart-define=BUILD_TYPE=enterprise --dart-define=FLAVOR_NAME=env1 --dart-define=AUTH_CUSTOM_SCHEME=eeflutter --dart-define-from-file=<env1_file>"
         ;;
-    bf2)
-        ARGS="--dart-define=BUILD_TYPE=enterprise --dart-define=FLAVOR_NAME=Bf2 --dart-define=AUTH_CUSTOM_SCHEME=eeflutter --dart-define-from-file=assets/env_files_dev/env_bf2.json"
+    env2)
+        ARGS="--dart-define=BUILD_TYPE=enterprise --dart-define=FLAVOR_NAME=env2 --dart-define=AUTH_CUSTOM_SCHEME=eeflutter --dart-define-from-file=<env2_file>"
         ;;
     prod)
-        ARGS="--dart-define=BUILD_TYPE=appstore --dart-define-from-file=assets/env_files_dev/env_prod_dev.json"
+        ARGS="--dart-define=BUILD_TYPE=appstore --dart-define-from-file=<prod_file>"
         ;;
     *)
         echo "Invalid choice. Exiting."
@@ -80,5 +82,5 @@ case $ARG_CHOICE in
         ;;
 esac
 
-# Run the Flutter app with the selected arguments
+# Run the app on the selected simulator with the chosen arguments
 run_flutter_app "$SIMULATOR_ID" $ARGS
